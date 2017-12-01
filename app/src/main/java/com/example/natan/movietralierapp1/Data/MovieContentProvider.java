2ea3e113d2e6eb14786575eed704c0dc7b4c5a09
Natansh <natansh25@gmail.com>
2017-12-01 12:06:00 -0500

    Data loaded into the database

---
+++ app/src/main/java/com/example/natan/movietralierapp1/Data/MovieContentProvider.java
@@ -0,0 +1,106 @@
+package com.example.natan.movietralierapp1.Data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by natan on 12/1/2017.
+ */
+
+public class MovieContentProvider extends ContentProvider {
+
+    private DbHelper mDbHelper;
+
+    public static final int MOVIES = 100;
+    public static final int MOVIE_WITH_ID = 101;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher() {
+
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        uriMatcher.addURI(Contract.AUTHORITY, Contract.PATH_TASKS, MOVIES);
+        uriMatcher.addURI(Contract.AUTHORITY, Contract.PATH_TASKS + "/#", MOVIE_WITH_ID);
+
+        return uriMatcher;
+
+
+    }
+
+
+    @Override
+    public boolean onCreate() {
+
+        Context context = getContext();
+        mDbHelper = new DbHelper(context);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+
+
+        final SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
+        Uri returnUri; // URI to be returned
+
+        switch (match) {
+            case MOVIES:
+
+                // Insert new values into the database
+                // Inserting values into tasks table
+                long id = db.insert(Contract.Entry.TABLE_NAME, null, values);
+                if ( id > 0 ) {
+                    returnUri = ContentUris.withAppendedId(Contract.Entry.CONTENT_URI, id);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            // Set the value for the returnedUri and write the default case for unknown URI's
+            // Default case throws an UnsupportedOperationException
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        // Notify the resolver if the uri has been changed, and return the newly inserted URI
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Return constructed uri (this points to the newly inserted row of data)
+        return returnUri;
+
+
+
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
